93d1f499423d5a4f30051c6ca65184940e11c304
warutniy <125255821+warutniy@users.noreply.github.com>
2023-03-13 13:48:45 +0700

    Add files via upload

---
+++ week_translator.js
@@ -0,0 +1,33 @@
+
+const dayNumber = Math.floor(Math.random() *10);
+
+let dayName;
+
+switch (dayNumber) {
+    case 0:
+        dayName = 'Sunday';
+        break;
+    case 1:
+        dayName = 'Monday';
+        break;
+    case 2:
+        dayName = 'Tuesday';
+        break;
+    case 3:
+        dayName = 'Wednesday';
+        break;
+    case 4:
+        dayName = 'Thursday';
+        break;
+    case 5:
+        dayName = 'Friday';
+        break;
+    case 6:
+        dayName = 'Saturday';
+        break;
+    default:
+        dayName = 'invalid day number';
+        break;
+}
+
+console.log(`Day Number is ${dayNumber}, so day name is ${dayName}`);
